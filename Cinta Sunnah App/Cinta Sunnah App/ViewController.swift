d2fa3719351a7a89336564d28246fc9b2a27a7a0
Abi SA <abisa3002@gmail.com>
2017-12-02 16:27:15 +0700

    Done

---
+++ Cinta Sunnah App/Cinta Sunnah App/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Cinta Sunnah App
+//
+//  Created by Abi Sayuti on 12/2/17.
+//  Copyright © 2017 Abi Sayuti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var CintaSunnah: UIWebView!
+    
+     let urlweb = "http://cintasunnah.com/"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+        let requesturl = NSURL(string: urlweb)
+        
+        
+        let request = NSURLRequest(url: requesturl! as URL)
+        CintaSunnah.loadRequest(request as URLRequest)
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
